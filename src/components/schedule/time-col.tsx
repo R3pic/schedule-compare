81096498a06e73bf195ccedcbbd5c060a94fae8d
R3pic <101osc@naver.com>
2025-02-23 22:56:39 +0900

    fix: 시간 가독성 수정

--- src/components/schedule/time-col.tsx
+++ src/components/schedule/time-col.tsx
@@ -1,12 +1,15 @@
 import {PERIOD} from '@schedule/constants.ts';
 import {ScheduleUtil} from '@schedule/utils.ts';
+import {useIsMobile} from '@components/hook/use-modile.tsx';
 
 export default function TimeCol() {
+  const isMobile = useIsMobile();
+
   return (
     <div className="flex flex-col">
       {PERIOD.map((period, i) => (
-        <div key={i} className="h-12 border border-black flex items-center justify-center">
-          <p className="text-xs">{ScheduleUtil.periodToTime(period)}:00</p>
+        <div key={i} className="h-12 border border-black flex justify-end">
+          <p className={isMobile ? 'text-xs' : 'text-sm'}>{ScheduleUtil.periodToTime(period)}:00</p>
         </div>
       ))}
     </div>
